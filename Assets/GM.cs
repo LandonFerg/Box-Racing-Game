7357d15d3f4cf835248f7d51c57647a94b773b97
LandonF <landonljf@gmail.com>
2018-02-22 13:20:06 -0500

    Intial Commit

---
+++ Assets/GM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour
+{
+
+    //public Driver driveScript1;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void resetScene()
+    {
+        Scene loadedLevel = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadedLevel.buildIndex);
+    }
+}
+
---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    public Rigidbody[] rb;
+    public CapsuleCollider playerColl;
+    public GameObject cam;
+    public WheelDrive drive;
+    public Driver CC;
+    public GameObject carRoot;
+    public Transform carPrefab;
+    public bool dead;
+    public bool collided;
+    public UnityStandardAssets.Cameras.AutoCam camScript;
+    //public AutoCam camScript;
+    void Start()
+    {
+       // carRoot.SetActive(true);
+        dead = false;   // Alive at start
+
+        rb = GetComponentsInChildren<Rigidbody>();
+        camScript = cam.GetComponent<UnityStandardAssets.Cameras.AutoCam>();
+        for (int i = 0; i < rb.Length; i++)
+        {
+            var rigidbodies = rb[i];
+
+            rigidbodies.isKinematic = true;
+            rigidbodies.detectCollisions = false;
+            playerColl.enabled = true;
+            camScript.enabled = true;
+            CC.enabled = true;
+            drive.enabled = true;
+        }
+
+    }
+
+
+    void OnTriggerEnter(Collider dataFromCollision)
+    {
+        if (dataFromCollision.gameObject.tag == "ground")
+        {
+            StartCoroutine(Reset());
+            rb = GetComponentsInChildren<Rigidbody>();
+            Debug.Log("COLLIDING");
+
+            for (int i = 0; i < rb.Length; i++)
+            {
+                var rigidbodies = rb[i];
+
+                rigidbodies.isKinematic = false;
+                rigidbodies.detectCollisions = true;
+                playerColl.enabled = false;
+                camScript.enabled = false;
+                CC.enabled = false;
+                drive.enabled = false;
+                dead = true;
+                collided = true;    // debug
+            }
+        }
+        else { }
+
+    }
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(5);
+        Debug.Log("RESPAWNING");
+
+        Instantiate(carPrefab, new Vector3(10.4f, 244, -1060), Quaternion.identity);
+        rb = GetComponentsInChildren<Rigidbody>();
+        for (int i = 0; i < rb.Length; i++)
+        {
+            var rigidbodies = rb[i];
+
+            rigidbodies.isKinematic = true;
+            rigidbodies.detectCollisions = false;
+        }
+        carRoot.SetActive(false);
+    }
+}
